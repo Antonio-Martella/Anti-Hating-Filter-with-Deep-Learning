9f46c3745f8d714aa1c85be1dd41afeaa921d476
NNS0ILNOME <zicr97@gmail.com>
2025-11-22 10:08:47 +0000

    updates

--- src/data_utils.py
+++ src/data_utils.py
@@ -61,9 +61,62 @@ def split_dataset_binary(df, test_size=0.2, val_size = 0.2, stratify=True, augme
     )
 
     os.makedirs('data/binary_hate', exist_ok=True)
-    train_df.to_csv("data/binary_hate/train_binary_hate.csv", index=False)
-    test_df.to_csv("data/binary_hate/test_binary_hate.csv", index=False)
-    val_df.to_csv("data/binary_hate/val_binary_hate.csv", index=False)
+    (train_df.drop(columns='has_hate', errors="ignore").to_csv("data/binary_hate/train_binary_hate.csv", index=False))
+    (test_df.drop(columns='has_hate', errors="ignore").to_csv("data/binary_hate/test_binary_hate.csv", index=False))
+    (val_df.drop(columns='has_hate', errors="ignore").to_csv("data/binary_hate/val_binary_hate.csv", index=False))
+
+    print(val_df.columns)
+
+    if augmentation:
+        augmented_rows = []
+        for _, row in train_df.iterrows():
+            s = int(row['sum_injurious'])
+            if s <= 1:
+                repeat_n = 1
+            else:
+                repeat_n = s 
+            for _ in range(repeat_n):
+                augmented_rows.append(row.copy())
+        train_aug = pd.DataFrame(augmented_rows)
+        train_aug = train_aug.sample(frac=1, random_state=1).reset_index(drop=True)
+    else:
+        train_aug = train_df.copy()
+
+    return train_aug, test_df, val_df
+
+
+#------------------------------------------------------------------
+
+
+def split_dataset_type(df, test_size=0.2, val_size = 0.2, stratify=True, augmentation=False):
+
+    df['has_hate'] = (df['sum_injurious'] > 0).astype(int)
+
+    class_counts = df['has_hate'].value_counts().sort_index()
+    evaluation_class(count=class_counts, folder='binary_hate')
+
+    train_df, test_df = train_test_split(
+        df,
+        test_size=test_size,
+        random_state=1,
+        stratify=df['has_hate'] if stratify else None,
+        shuffle=True
+    )
+
+    train_df, val_df = train_test_split(
+        train_df,
+        test_size=test_size,
+        random_state=1,
+        stratify=train_df['has_hate'] if stratify else None,
+        shuffle=True
+    )
+
+    os.makedirs('data/binary_hate', exist_ok=True)
+    (train_df.drop(columns='has_hate', errors="ignore").to_csv("data/binary_hate/train_binary_hate.csv", index=False))
+    (test_df.drop(columns='has_hate', errors="ignore").to_csv("data/binary_hate/test_binary_hate.csv", index=False))
+    (val_df.drop(columns='has_hate', errors="ignore").to_csv("data/binary_hate/val_binary_hate.csv", index=False))
+
+    print(val_df.columns)
 
     if augmentation:
         augmented_rows = []
