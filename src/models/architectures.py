f5af885175b5dfbb08fb169ae8674f927e3f9394
Antonio-Martella <zicr97@gmail.com>
2025-11-25 08:27:16 +0000

    updates

---
+++ src/models/architectures.py
@@ -0,0 +1,56 @@
+from tensorflow.keras.layers import Embedding, Dense, LSTM, Bidirectional, Dropout, BatchNormalization, GlobalMaxPooling1D, Layer
+from tensorflow.keras.models import Sequential
+from attention_layer import AttentionLayer
+
+
+def binary_hate_model(vocabulary_size, max_len, dropout, optimizer, loss, metrics):
+
+  model = Sequential()
+  model.add(Embedding(input_dim = vocabulary_size, output_dim = 64, input_length = max_len))
+
+  #
+  model.add(Bidirectional(LSTM(32, return_sequences=True, activation='tanh')))
+  model.add(AttentionLayer())
+  model.add(BatchNormalization())
+  model.add(Dropout(dropout))
+
+  #
+  model.add(Dense(16, activation='relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(dropout))
+
+  model.add(Dense(units = 1, activation = 'sigmoid'))
+  model.build(input_shape = (None, max_len))
+
+  model.compile(optimizer = optimizer,
+                loss = loss,
+                metrics = metrics)
+
+  return model
+
+# ----------------------------
+
+def hate_type_model(vocabulary_size, max_len, dropout, optimizer, loss, metrics):
+
+  model = Sequential()
+  model.add(Embedding(input_dim = vocabulary_size, 
+                      output_dim = 128, 
+                      input_length = max_len))
+
+  model.add(Bidirectional(LSTM(units = 64, return_sequences=True, activation = 'tanh')))
+  model.add(AttentionLayer())
+  model.add(BatchNormalization())
+  model.add(Dropout(dropout))
+
+  model.add(Dense(units = 32, activation = 'relu'))
+  model.add(BatchNormalization())
+  model.add(Dropout(dropout))
+
+  model.add(Dense(units = 6, activation = 'sigmoid'))
+  model.build(input_shape = (None, max_len))
+
+  model.compile(optimizer = optimizer,
+                loss = loss,
+                metrics = metrics)
+
+  return model
