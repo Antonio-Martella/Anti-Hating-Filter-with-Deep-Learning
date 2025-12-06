f61735c573feef934ef3813c8a21f8cd6fc649f3
Antonio-Martella <zicr97@gmail.com>
2025-12-06 22:35:00 +0000

    updates

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,7 +2,6 @@ from .architectures import binary_hate_model, hate_type_model
 from .callbacks import callback_binary_hate, callback_hate_type
 from .losses import weighted_binary_crossentropy
 from .class_weights import class_weights_hate, compute_class_weights
-from .losses import weighted_binary_crossentropy
 from .attention_layer import AttentionLayer
 
 __all__ = [
@@ -13,6 +12,5 @@ __all__ = [
     "weighted_binary_crossentropy",
     "class_weights_hate",
     "compute_class_weights", 
-    "weighted_binary_crossentropy",
     "AttentionLayer"
 ]
--- src/predict.py
+++ src/predict.py
@@ -3,14 +3,16 @@ import pandas as pd
 import tensorflow as tf
 import json
 import pickle
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from tensorflow.keras.models import load_model
-#from model import weighted_binary_crossentropy
 from data_utils import  preprocess_text
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-from models import AttentionLayer
-from utils import F1Score
+from models import weighted_binary_crossentropy
 
 
 # -----------------------------
@@ -19,11 +21,7 @@ from utils import F1Score
 # LOAD THE MODEL
 print("\033[92m------ First Model ------\033[0m")
 try:
-  model_binary_hate = load_model('models/binary_hate/model_hate_binary.keras')
-  custom_objects={
-        "AttentionLayer": AttentionLayer
-        #"F1Score": F1Score
-        }
+  model_binary_hate = load_model('models/binary_hate/model_hate_binary.h5')
   print(f"\033[92mFirst Model (model_binary_hate.keras) loaded successfully!\033[0m")
 except Exception as e:
   print(f"\033[91mError loading First Model (model_binary_hate.keras): {e}!\033[0m")
@@ -59,54 +57,110 @@ except Exception as e:
 # -------- SECOND MODEL --------
 # ------------------------------
 # LOAD THE TENSOR WEIGHTS FOR THE 'model_hate_type'loaded_weights = np.load('results/hate_type/weights_tensor.npy')
-'''weights_tensor = tf.constant(loaded_weights, dtype=tf.float32)
+loaded_weights = np.load('models/hate_type/weights_tensor.npy', allow_pickle=True)
+weights_tensor = tf.constant(loaded_weights, dtype=tf.float32)
 
 # Load the second model
 try:
   model_hate_type = load_model(
-    #'/models/hate_type/model_hate_type.h5',
-    '/content/drive/MyDrive/Colab Notebooks/Progetto GitHub/DL GitHub/model_hate_type.h5',
+    'models/hate_type/model_hate_type.h5',
     custom_objects={
-      "AttentionLayer": AttentionLayer,
+      #"AttentionLayer": AttentionLayer,
       "weighted_binary_crossentropy": weighted_binary_crossentropy(weights_tensor)
-      },
-      compile=False
-    )
+    },
+    compile=False
+  )
   print(f"\033[92mModel 'model_hate_type.h5' loaded successfully\033[0m")
 except Exception as e:
   print(f"\033[91mError loading model 'model_hate_type.h5': {e}\033[0m")
 
 # Load the optimal threshold for the first model
-with open('results/binary_hate/best_threshold.json', 'r') as f:
-  best_threshold_binary_hate = json.load(f)["threshold"]
+with open('models/hate_type/best_thresholds_hate_type.json', 'r') as f:
+  best_thresholds_hate_type = json.load(f)
 
 # Load the optimal threshold for the first model
-with open('models/binary_hate/tokenizer_param_binary_hate.json', 'r') as f:
-  max_len_bin_hate = json.load(f)["max_len"]
+with open('models/hate_type/param_tokenizer_hate_type.json', 'r') as f:
+  max_len_hate_type = json.load(f)["max_len"]
 
 # Load the tokenizer for the first model
 try:
-  with open("models/binary_hate/tokenizer_binary_hate.pkl", "rb") as f:
-      tokenizer_binary_hate = pickle.load(f)
+  with open("models/hate_type/tokenizer_hate_type.pkl", "rb") as f:
+      tokenizer_hate_type = pickle.load(f)
 except Exception as e:
-  print("Errore nel caricamento del tokenizer:", e)'''
+  print("Error loading second model tokenizer:", e)
 
 
-df = pd.read_csv('data/test_comments.csv')
+while True:
+    text = input("Inserisci una frase ('exit' per uscire): ")
+
+    if text.lower() == "exit":
+        break
+    
+    seq = tokenizer_binary_hate.texts_to_sequences([text])
+    pad = pad_sequences(seq, maxlen=max_len_binary_hate)
+    prob = model_binary_hate.predict(pad)[0][0]
+    pred = int(prob >= best_threshold_binary_hate['best_thresholds_has_hate'])
+
+    #print("Probabilità di hating:", prob)
+    #print("Predizione:", pred)
+    
+    if pred == 1:
+      seq = tokenizer_hate_type.texts_to_sequences([text])
+      pad = pad_sequences(seq, maxlen=max_len_hate_type)
+      prob = model_hate_type.predict(pad)[0]
+      
+      pred_labels = []
+      labels_hate_type = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+      for i, label in enumerate(labels_hate_type):
+          threshold = best_thresholds_hate_type[f"best_thresholds_{label}"]
+          pred_labels.append(int(prob[i] >= threshold))
+      print(pred_labels)
+    else:
+      print([0, 0, 0, 0, 0, 0])
+
+
+## COSA RIMANE DA FARE:
+## Creare un dataset come soli commenti, che viene passato
+## al preprocess del testo (da implementare) e poi fornisce
+## un file in unscita che identifica le labels, fare poi 
+## un altro file di inferenza ma che accetta volta per volta
+## dei commenti (vedere come implementare il preprocess del testo)
+
+
+'''import numpy as np
+import pandas as pd
+
+# Carica dataset
+df = pd.read_csv('data/train_and_test/test_dataset.csv')
 df = preprocess_text(df, text_col="comment_text")
 
-X = df[df["sum_injurious"] >= 1]
-print(len(X))
+# Dividi tra hate / non-hate
+df_hate = df[df["sum_injurious"] > 0]
+df_nonhate = df[df["sum_injurious"] == 0]
 
-X_sequences = tokenizer_binary_hate.texts_to_sequences(X["comment_text"].astype(str))
+# Seleziona 10 esempi casuali per ciascun gruppo
+sample_hate = df_hate.sample(n=10, random_state=42)
+sample_nonhate = df_nonhate.sample(n=10, random_state=42)
 
-padded_X_sequences = pad_sequences(
+# Unisci in un unico df
+samples = pd.concat([sample_hate, sample_nonhate]).reset_index(drop=True)
+
+# --- PREPROCESSING PER IL MODELLO ---
+
+X_sequences = tokenizer_binary_hate.texts_to_sequences(samples["comment_text"].astype(str))
+
+padded_sequences = pad_sequences(
     sequences=X_sequences,
     maxlen=int(max_len_binary_hate)
 )
 
-y_pred = model_binary_hate.predict(padded_X_sequences)
+# Predizioni
+y_pred = model_binary_hate.predict(padded_sequences)
 y_pred_opt = (y_pred >= best_threshold_binary_hate["best_thresholds_has_hate"]).astype(int).flatten()
 
-for i in range(50):
-    print(X.sum_injurious.values[i], y_pred_opt[i])
+# --- STAMPA RISULTATI ---
+for text, inj, pred in zip(samples["comment_text"], samples["sum_injurious"], y_pred_opt):
+    print("---------------")
+    print("TESTO:", text[:200], "..." if len(text) > 200 else "")
+    print("sum_injurious:", inj)
+    print("Predizione modello:", pred)'''
